aa5b5b60a3552e81c43f0d3656a2400cd6286594
Aakif Nehal <aakifnehal@gmail.com>
2025-08-27 16:41:20 +0530

    Refactor code structure and remove redundant functions for improved readability and maintainability

--- server/main.py
+++ server/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
 from auth.routes import router as auth_router
+from docs.routes import router as docs_router
+from chat.routes import  router as chat_router
 
-app = FastAPI()
+app=FastAPI()
 
 app.include_router(auth_router)
+app.include_router(docs_router)
+app.include_router(chat_router)
 
 @app.get("/health")
 def health_check():
-    return {"status": "healthy"}
-
-def main():
-    print("Hello from server!")
-
-
-if __name__ == "__main__":
-    main()
+    return {"message":"OK"}
